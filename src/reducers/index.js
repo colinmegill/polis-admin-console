3f98957c2d95a8652647e4c773e5e01527fcd444
Mike Bjorkegren <m@bjorkegren.com>
2016-05-15 02:33:25 -0700

    add copyright notice to all files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2012-present, Polis Technology Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights for non-commercial use can be found in the PATENTS file
+ * in the same directory.
+ */
+
 import { combineReducers } from "redux";
 import conversations from "./conversations";
 import user from "./user";
--- src/strings/en_us.js
+++ src/strings/en_us.js
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2012-present, Polis Technology Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights for non-commercial use can be found in the PATENTS file
+ * in the same directory.
+ */
+
 
 var s = {};
 
